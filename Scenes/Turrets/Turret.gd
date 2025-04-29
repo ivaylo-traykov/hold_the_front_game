de6fcaba53fcb2de668ad6f1dd1fd51e5dd5a3a7
ivaylo-traykov <ivailotraikov@abv.bg>
2025-04-29 12:47:35 +0300

    Turrets can detect enemies and rotate according to their position

--- Scenes/Turrets/Turret.gd
+++ Scenes/Turrets/Turret.gd
@@ -9,6 +9,8 @@ class_name Turret extends Node2D
 @onready var range_overlay: Sprite2D = $Range/RangeOverlay
 
 var built: bool = false
+var enemies: Array[Enemy]
+var target: PathFollow2D = null
 
 func _ready() -> void:
 	set_range(stats.range)
@@ -18,23 +20,43 @@ func _ready() -> void:
 func _process(_delta) -> void:
 	if not built:
 		return
-		
-	turn()
-
-
-func turn() -> void:
-	var enemy_position = get_global_mouse_position()
-	turret_sprite.look_at(enemy_position)	
+	if enemies.size() > 0:
+		if not target:
+			target = select_enemy()
+		else:
+			turn()
 
 
 #region Helper Functions
+func select_enemy() -> PathFollow2D:
+	var target: PathFollow2D
+	if enemies.size() <= 0:
+		return null
+	return enemies[0]
+
+
+func turn() -> void:
+	var enemy_position = select_enemy().get_global_position()
+	turret_sprite.look_at(enemy_position)
+
+
 func build() -> void:
 	built = true
 	range_overlay.set_visible(false)
-	
+
 
 func set_range(range) -> void:
 	var scaling = range / 600.0
 	self.range.get_shape().set_radius(range / 2)
 	self.range_overlay.scale = Vector2(scaling, scaling)
+#endregion
+
+
+#region Range Signals
+func _on_range_body_entered(body):
+	enemies.append(body.get_parent())
+
+
+func _on_range_body_exited(body):
+	enemies.erase(body.get_parent())
 #endregion
